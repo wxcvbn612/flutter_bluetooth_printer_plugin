9bf8d5b60a02c8192729a248441fa68563aedd3a
Jait Aimad <jait.aimad@gmail.com>
2024-06-23 18:01:15 +0100

    Add 80MM size

--- packages/flutter_bluetooth_printer/lib/src/flutter_bluetooth_printer_impl.dart
+++ packages/flutter_bluetooth_printer/lib/src/flutter_bluetooth_printer_impl.dart
@@ -8,6 +8,7 @@ class DiscoveryResult extends DiscoveryState {
 enum PaperSize {
   // original is 384 => 48 * 8
   mm58(360, 58, 'Roll Paper 58mm');
+  mm80(497, 80, 'Roll Paper 58mm');
 
   final int width;
   final double paperWidthMM;
